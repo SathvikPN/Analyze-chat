f8e7fd9493d8ba2c1931b9b769bddfa6bdfcdc42
Sathvik PN <sathvikapn@gmail.com>
2021-07-15 00:01:47 +0530

    add username format pattern

--- src/settings.py
+++ src/settings.py
@@ -12,4 +12,10 @@ CONTACT_LINKEDIN = 'https://www.linkedin.com/in/sathvik-p-n/'
 PATTERN = {
     'date-time': '^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]?(AM|PM|am|pm)? -',
     'URL': '(?:(?:(?:https|http|ftp):\/\/(?:www\.)?)|(?:www\.))((?:[\w\-_]+?\.)?(?:[\w\-_]+?\.)?[\w\-_]+?\.\w+)\S*',
+    'username': [
+        '([\w]+):',                     # First_name (1+ alphanumeric characters)
+        '([\w]+)[\s]+([\w]+):',         # fname lname
+        '([\w]+[\s]+[\w]+[\s]+[\w]+):', # fname Mname lname
+        '([+]\d{2} \d{5} \d{5}):',      # mobile number (India)
+    ]
 }
